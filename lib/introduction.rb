82cf116a2cce05728ae58c09cc557e042f155b43
Janice Pena <jipena94@gmail.com>
2019-02-22 04:30:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,4 +7,5 @@ end
 #introduction_with_language
 
 def introduction_with_language
-  puts "Hi, my name is #{name} and I am learning to program in Ember"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
