e10fbe0bbc194f69f1ce8f83e2c4859686262d18
Janice Pena <jipena94@gmail.com>
2019-02-22 04:38:21 +0000

    Automatically backed up by Learn

--- introduction_with_language.rb
+++ introduction_with_language.rb
@@ -1,5 +1,5 @@
 # #introduction_with_language.rb
  
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}."
+  puts `"Hi, my name is #{name} and I am learning to program in #{language}."
 end
